3ac899175952e4655ededcc277e5af3e85bd6e0c
dipanjanbera <dipanjan033@gmail.com>
2020-08-09 01:24:43 +0530

    commit

---
+++ commentInPython.py
@@ -0,0 +1,5 @@
+#This is a sample one line comment
+"""
+This ia example of Multiline comment
+where more than one line can be commented
+"""
---
+++ variables.py
@@ -0,0 +1,18 @@
+x = 10
+y = 15.89
+z = 18 + 8j
+a = "Str"
+b = True
+b = False
+print("x is a type ", type(x))
+print("y is a type ", type(y))
+print("z is a type ", type(z))
+print("a is a type ", type(a))
+print("b is a type ", type(b))
+#Type conversion
+toFloat = float(x)
+toInt = int(y)
+toComplex = complex(y)
+print("After Conversion of x to Float,Value is ", toFloat)
+print("After Conversion of y to int,Value is ", toInt)
+print("After Conversion of y to Complex,Value is ", toComplex)
